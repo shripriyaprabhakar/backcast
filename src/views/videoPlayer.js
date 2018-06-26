3e6016763739ac06acf5c974c634836a29a3f331
shripriyaprabhakar <shripriyaprabhakar@gmail.com>
2018-06-26 12:37:48 -0700

    Add initialize function, change render behavior

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,17 +1,23 @@
 var VideoPlayerView = Backbone.View.extend({
  
+  el: '.video-player',
 
   initialize: function() {
-    this.collection.on('change', this.render, this);
-
+    this.collection.on('select', function() {
+      this.render();
+    }, this);
+    
   },
 
+  
+
   render: function() {
-    this.$el.html(this.template());
-    return this;
+    this.$el.html(this.template(/*PASSS SOMETHING HERE*/));
+    console.log(this.collection);
+    return this.$el;
     
   },
   
   template: templateURL('src/templates/videoPlayer.html')
-  //$('.body')append.()
+  
 });
