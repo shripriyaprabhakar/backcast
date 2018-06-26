ec919ac700e9c5ba357419e5974fa428883f33cb
shripriyaprabhakar <shripriyaprabhakar@gmail.com>
2018-06-25 21:34:09 -0700

    Add initialize function (maybe need to fix)

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,9 +1,18 @@
 var VideoPlayerView = Backbone.View.extend({
+ 
 
+  initialize: function() {
+    /*this.collection.on('select', function() {
+      this.render();
+    }, this);*/
+
+  },
 
   render: function() {
     this.$el.html('<div class="loading">Please wait...</div>');
     return this;
+    
+
   },
 
   template: templateURL('src/templates/videoPlayer.html')
