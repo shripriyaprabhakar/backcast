7defc3690bc8abb702b6f1bfcab09b35bde969b8
shripriyaprabhakar <shripriyaprabhakar@gmail.com>
2018-06-25 21:32:30 -0700

    Implement render function of VideoListView

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,9 +1,19 @@
 var VideoListView = Backbone.View.extend({
+  
+  el: '.video-list',
 
+  initialize: function() {
+    // videos is a collection, whose model is Video
+  },
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+    this.$el.html(this.collection.map(function(video) {
+      return new VideoListEntryView({
+        model: video
+      }).render().el;
+    }));
     return this;
   },
 
