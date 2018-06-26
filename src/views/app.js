84a7d32cd81adeeaddccdafdc062c34223c5623b
shripriyaprabhakar <shripriyaprabhakar@gmail.com>
2018-06-25 21:26:22 -0700

    Implement rendering of the three views within AppView

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,31 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    // videos is a collection, whose model is Video
+    this.videos = new Videos(window.exampleVideoData);
+    this.render();
+    
   },
 
 
   render: function() {
+    // renders the html from the link below
     this.$el.html(this.template());
+    // render searchView
+    new SearchView({
+      el: this.$('.search'),
+      collection: this.videos
+    }).render();
+    // render videoPlayerView
+    new VideoPlayerView({
+      el: this.$('.player'),
+      collection: this.videos
+    }).render();
+    // render videoListView
+    new VideoListView({
+      el: this.$('.list'),
+      collection: this.videos
+    }).render();
     return this;
   },
 
